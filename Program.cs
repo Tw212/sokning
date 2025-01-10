d48dd610eafa29dac0c75c636631f046cc08cb85
Tw212 <Tewe07001@utb.vaxjo.se>
2025-01-10 13:58:28 +0100

    yfry

--- Program.cs
+++ Program.cs
@@ -1,4 +1,38 @@
-﻿
+﻿using drtdr;
+static int Sokning(List<Person> list, int key){
+    int high = list.Count-1;
+    int low = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+                if(key> mid){
+                    low= mid + 1;
+                }
+                else if(key< mid){
+                    low = mid - 1;
+                }
+                else return mid;
+        }
+        return -1;
+}
 List<Person> personlista = new List<Person>();
 personlista.Add(new Person(1, "teodor"));
+personlista.Add(new Person(2,"Jetmir Curry"));
+personlista.Add(new Person(3,"Tore"));
+personlista.Add(new Person(4,"Andis"));
+personlista.Add(new Person(5,"Tore"));
+personlista.Add(new Person(6,"Tore"));
+personlista.Add(new Person(7,"Tore"));
+personlista.Add(new Person(8,"Tore"));
+personlista.Add(new Person(9,"Tore"));
+personlista.Add(new Person(10,"Tore"));
+personlista.Add(new Person(11,"Tore"));
+personlista.Add(new Person(12,"Tore"));
+personlista.Add(new Person(13,"Tore"));
+personlista.Add(new Person(14,"Tore"));
+personlista.Add(new Person(15,"Tore"));
+
+Console.WriteLine("Skriv in person nummer");
+int nyckel = int.Parse(Console.ReadLine());
+int index = Sokning(personlista, nyckel);
+Console.WriteLine(personlista[index].Namn);
 
--- obj/Debug/net8.0/drtdr.AssemblyInfo.cs
+++ obj/Debug/net8.0/drtdr.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("drtdr")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+056af658f07019e9b35f4555ef89ece7cc124d2a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc97f3f5e3fc7c6f5d5f8cfabef3be450e8da6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("drtdr")]
 [assembly: System.Reflection.AssemblyTitleAttribute("drtdr")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
